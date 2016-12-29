55a78d17b2da2873d76723e014d95818a08d40cd
sumitbsn <sumitbsn@gmail.com>
2016-12-30 00:10:17 +0530

    library-dependency header

---
+++ libdependency.h
@@ -0,0 +1,13 @@
+#ifndef LIBDEPENDENCY_H
+#define LIBDEPENDENCY_H
+#include <stdio.h>
+#include <malloc.h>
+#include <memory.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <libgen.h>
+#include "hashtable.h"
+#define SIZE 500
+
+#endif
